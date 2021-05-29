3e60610fe926eae46f4dd42ac9fcb6d7f56f5136
Vhyrro <vhyrro@gmail.com>
2021-05-29 13:47:38 +0200

    Rework: make cheovim actually usable

--- lua/cheovim.lua
+++ lua/cheovim.lua
@@ -1,5 +1,10 @@
-local cheovim = {}
+local loader, log = require('cheovim.loader'), require('cheovim.logger')
 
-require('cheovim.loader').get_profiles(vim.fn.stdpath("config") .. "/profiles.lua")
+local selected_profile, profiles = loader.get_profiles(vim.fn.stdpath("config") .. "/profiles.lua")
 
-return cheovim
+if not selected_profile then
+	log.error("Failed to load cheovim: profile could not be found.")
+	return
+end
+
+loader.create_plugin_symlink(selected_profile, profiles)
